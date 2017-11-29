852e292ba95e14d02dc17043071c14617b600781
mbApple <983140171@qq.com>
2017-11-29 14:41:49 +0800

    starView finish
    starView 完成版

--- KiStarRateView/KiStarRateView/KiStarRateView.swift
+++ KiStarRateView/KiStarRateView/KiStarRateView.swift
@@ -16,21 +16,81 @@ import UIKit
 
 class KiStarRateView: UIView {
     //MARK : public
-    public var scorePercent:CGFloat = 0 //得分值，范围为0--1，默认为1
+    public var scorePercent:CGFloat = 1{//得分值，范围为0--1，默认为1
+        didSet{
+            if self.scorePercent < 0 {
+                self.scorePercent = 0
+            }else if self.scorePercent > 1{
+                self.scorePercent = 1
+            }
+            
+            if self.delegate != nil && delegate?.starRateView != nil {
+                self.delegate?.starRateView!(self, newScorePercent: scorePercent)
+            }
+            
+            self.setNeedsLayout()
+        }
+    }
     public var hasAnimation:Bool = false  //是否允许动画，默认为false
     public var allowIncompleteStar:Bool = false //评分时是否允许不是整星，默认为NO
     public weak var delegate:KiStarRateViewDelegate?  //代理
     public func setBottomImageName(_ bottomImgName:String,topImgName:String,WJXCount:NSInteger) {
-        
+        self.bottomImageName = bottomImgName
+        self.topImageName = topImgName
+        self.numberOfStars = WJXCount
+        buildDataAndUI()
     }
     
     //MARK : private
-    
+    private let Defalut_Star_Number = 5 //默认星星的个数
+    private let Animation_Time_Tnterval = 0.2 //动画时间
     private var foregroundStarView = UIView()
     private var backgroundStarView = UIView()
     private var numberOfStars = 0
     private var bottomImageName = ""
     private var topImageName = ""
+    
+    private func buildDataAndUI() {
+        scorePercent = 1 //默认
+        hasAnimation = false
+        allowIncompleteStar = false
+        self.foregroundStarView = self.createStarViewWithImage(imageName: self.topImageName)
+        self.backgroundStarView = self.createStarViewWithImage(imageName: self.bottomImageName)
+        self.addSubview(self.foregroundStarView)
+        self.addSubview(self.backgroundStarView)
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(userTapRateView(gesture:)))
+        tapGesture.numberOfTapsRequired = 1
+        self.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc private func userTapRateView(gesture:UITapGestureRecognizer){
+        let tapPoint = gesture.location(in: self)
+        let offset = tapPoint.x
+        let realStarScore = offset / (self.bounds.size.width / CGFloat(self.numberOfStars))
+        let starScore = self.allowIncompleteStar ? Float(realStarScore) : ceilf(Float(realStarScore))
+        self.scorePercent = CGFloat(starScore / Float(self.numberOfStars))
+    }
+    
+    private func createStarViewWithImage(imageName:String) -> UIView {
+        let view = UIView(frame: self.bounds)
+        view.clipsToBounds = true
+        view.backgroundColor = .clear
+        for index in 0..<self.numberOfStars {
+            let imageView = UIImageView(image: UIImage.init(named: imageName))
+            imageView.frame = CGRect(x: CGFloat(index) * self.bounds.size.width / CGFloat(self.numberOfStars), y: 0, width: self.bounds.size.width / CGFloat(self.numberOfStars), height: self.bounds.size.height)
+            imageView.contentMode = .scaleAspectFit
+            view.addSubview(imageView)
+        }
+        return view
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        let animationTimeInterval = self.hasAnimation ? self.Animation_Time_Tnterval : 0
+        UIView.animate(withDuration: animationTimeInterval) {[weak self] in
+            self?.foregroundStarView.frame = CGRect(x: 0, y: 0, width: (self?.bounds.size.width ?? 0)! * (self?.scorePercent ?? 0)!, height: (self?.bounds.size.height ?? 0)!)
+        }
+    }
     
 
     /*
--- KiStarRateView/KiStarRateView/ViewController.swift
+++ KiStarRateView/KiStarRateView/ViewController.swift
@@ -8,12 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,KiStarRateViewDelegate {
+    
+    lazy var starView: KiStarRateView = {
+        let starView = KiStarRateView(frame: CGRect(x: 0, y: 100, width: 375, height: 50))
+        starView.setBottomImageName("b27_icon_star_gray", topImgName: "b27_icon_star_yellow", WJXCount: 5)
+        starView.allowIncompleteStar = true  //是否允许, 整颗星
+        starView.hasAnimation = true  //动画
+        starView.scorePercent = 0.3  //默认评分  (0 - 1)
+        starView.delegate = self
+        return starView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = .white
+        
+        self.view.addSubview(self.starView)  //添加....五角星...
+        
     }
+    
+     //MARK ---- KiStarRateViewDelegate
+    func starRateView(_ starRateView: KiStarRateView, newScorePercent: CGFloat) {
+        print("newScorePercent======\(newScorePercent)")
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
